2764215384bd4129663fc2e0bcc2de28c8e6737a
DanAkrabi <danakrabi@gmail.com>
2025-04-02 13:37:30 +0300

    added front build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,11 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  apps: [
+    {
+      name: "REST SERVER",
+      script: "./dist/src/server.js", // הנתיב הנכון אחרי קומפילציה
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
